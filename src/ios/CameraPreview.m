7c492f7f0be8964174fbdbfc98091212a113e84b
mdiepenbrock <matthew@storyfilms.net>
2017-02-28 16:59:32 -0800

    Remove duplicate long-running method call that was running on the main thread

--- src/ios/CameraPreview.m
+++ src/ios/CameraPreview.m
@@ -71,7 +71,6 @@
     [self.cameraRenderController removeFromParentViewController];
     self.cameraRenderController = nil;
 
-    [self.sessionManager.session stopRunning];
     self.sessionManager = nil;
 
     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
